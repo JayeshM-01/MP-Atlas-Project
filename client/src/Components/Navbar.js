caf08eb4e9eba4733648cd301b3e5eee78927ca9
Naveen <146852997+NaveenSh7@users.noreply.github.com>
2024-03-22 00:35:52 +0530

    changes

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import Logo from '../Components/images/iitilogo.jpg';
 
 
 const Navbar= ()=> {
@@ -15,8 +16,10 @@ const Navbar= ()=> {
             title="Company"
             class="inline-flex items-center"
           >
-            {/* <svg
-              class="w-8 text-teal-accent-400"
+            <img
+            
+             src={Logo}
+              class="w-10 lg:w-16 text-teal-accent-400"
               viewBox="0 0 24 24"
               strokeLinejoin="round"
               strokeWidth="2"
@@ -24,16 +27,15 @@ const Navbar= ()=> {
               strokeMiterlimit="10"
               stroke="currentColor"
               fill="none"
-            >
-              <rect x="3" y="1" width="7" height="12" />
-              <rect x="3" y="17" width="7" height="6" />
-              <rect x="14" y="1" width="7" height="6" />
-              <rect x="14" y="11" width="7" height="12"
               
-              />
-            </svg> */}
-
-            <img src=''></img>
+            >
+                
+           
+       
+            </img>
+{/* 
+            <img src={Logo}   ></img> */}
+            
             <span class="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
               IIT INDORE
             </span>
