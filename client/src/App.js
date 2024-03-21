34b83b44cf226478775902da57920560d745450a
JayeshM-01 <jayashmore278@gmail.com>
2024-03-21 19:00:28 +0530

    react install

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import home from './Components/home';
 
 function App() {
   return (
     <div className="App">
-      
+        hello
     </div>
   );
 }
---
+++ client/src/Components/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function home() {
+  return (
+    <div className="App">
+        This is home page
+    </div>
+  );
+}
+
+export default home;
