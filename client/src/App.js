b498aa465a7ba6b8af7b29f2c58d389a6e97fac1
Naveen <146852997+NaveenSh7@users.noreply.github.com>
2024-03-21 22:48:25 +0530

    Footer by naveen , Navbar by raja

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-
-
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 function App() {
   return (
     <div className="App">
-
+    <Navbar/>
+    <div>Slider</div>
+    <div>Boxes</div>
+    <Footer/>
     </div>
   );
 }
--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,11 +1,49 @@
 import React from 'react';
 
-function Footer() {
-  return (
-    <div className="App">
-        This is Navbar
+ const Footer = ()=>{
+
+return(
+
+<div>
+  
+<footer class="footer-distributed bg-gray-900 text-white py-8 px-4 lg:px-24 flex flex-col lg:flex-row items-center">
+
+  <div class="footer-left lg:w-3/4 lg:mr-12">
+    <h3 class="text-lg md:text-3xl xl:text-4xl font-normal text-white mb-2">INDIAN INSTITUTE OF TECHNOLOGY <span class=" text-teal-500 font-medium">INDORE</span></h3>
+
+    <p class="footer-links mt-4 lg:mt-10 text-sm lg:text-base">
+      District Websites of Madhya Pradesh are powered by S3WaaS Framework of NIC, MeitY in collaboration with District Administration and IT Department, Govt. of Madhya Pradesh
+    </p>
+
+    <p class="footer-company-name text-gray-300 mt-8 lg:mt-12 text-sm lg:text-base">© 2021 All rights reserved by State Administration</p>
+  </div>
+
+  <div class="footer-center lg:w-1/4 flex flex-col items-center lg:items-start mt-8 lg:mt-0">
+    <div class="flex items-center mb-4 lg:mb-7">
+      <i class="fa fa-map-marker bg-gray-700 text-white text-2xl rounded-full w-10 h-10 flex items-center justify-center mr-3"></i>
+      <p  class="text-xs lg:text-sm"><span>Department of</span><br/><span class="text-teal-500 font-semibold">
+        Civil Engineering
+        </span></p>
     </div>
-  );
+
+    <div class="flex items-center mb-4 lg:mb-7">
+      <i class="fa fa-phone bg-gray-700 text-white text-2xl rounded-full w-10 h-10 flex items-center justify-center mr-3"></i>
+      <p class="text-xs lg:text-sm">+1.555.555.5555</p>
+    </div>
+
+    <div class="flex items-center ">
+      <i class="fa fa-envelope bg-gray-700 text-white text-2xl rounded-full w-10 h-10 flex items-center justify-center mr-3">
+      </i>
+      <p class="text-xs lg:text-sm"> civiloff@iiti.ac.in<a href="mailto:support@company.com"></a></p>
+    </div>
+  </div>
+
+</footer>
+
+
+</div>
+);
+  
 }
 
 export default Footer;
