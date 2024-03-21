e1793b66bd5a12f4a388b8f1e7611fd183fc11fd
Naveen <146852997+NaveenSh7@users.noreply.github.com>
2024-03-22 03:36:13 +0530

    changed overview box css

--- client/src/Components/MPdata.js
+++ client/src/Components/MPdata.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 export const Overview = () =>{
     return(
-        <div className='w-[80%] p-6 m-16 mx-auto border-2 shadow-lg rounded-lg  '>
-            <h1 className='font-bold text-4xl pb-4 text-indigo-900'>Overview of Madhya Pradesh</h1>
-            <p class="font-semibold text-emerald-600 text-2xl text-justify w-[80%] mx-auto ">
+        <div className='w-[80%] p-6 m-16 mx-auto border-2 shadow-lg bg-white rounded-3xl  '>
+          <div class="flex justify-center ">
+          <h1 className='font-bold max-w-fit text-4xl text-[#ffffff] bg-[#00ADB5] mb-6 px-4 border-black border-[1px] rounded-3xl flex justify-center p-2 '>Overview of Madhya Pradesh</h1>
+            </div> 
+            <p class="font-semibold text-black text-2xl text-justify w-[80%] mx-auto ">
                 Madhya Pradesh is resplendent with the hill ranges of the
                 Vindhyas and the Satpuras. Its natural settings beautified
                 by hills, forests, rivers, rich heritages, exciting wild life and
