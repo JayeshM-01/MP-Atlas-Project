c268a23683f37248842af130ade49624d12f7874
raja <rt12122003@gmail.com>
2024-03-22 00:33:32 +0530

    added Slider

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import Cards from './Components/Cards';
-
+import Slider from './Components/Slider';
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <Slider/>
     <Cards/>
     <Footer/>
     </div>
